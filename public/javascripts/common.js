1802ed8ac025f842caf3b814bd9fc1dd755bb350
xckouy <xckouy@gmail.com>
2013-05-31 15:55:07 +0800

    menu active

---
+++ public/javascripts/common.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+     $(".nav-list li").click(function(){
+         $(this).siblings().removeClass("active");
+         $(this).addClass("active");
+     });
+
+     
+ });
+ 
+
+ 
